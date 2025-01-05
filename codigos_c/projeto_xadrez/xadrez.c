6012b4cecd1c61ebf4e81983ffe680dacba70a6f
Bruna-righi <righi.bruna@acad.ufsm.br>
2025-01-05 13:57:54 -0300

    update: xadrez

--- codigos_c/projeto_xadrez/xadrez.c
+++ codigos_c/projeto_xadrez/xadrez.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 
 #define tam_tab 8
+#define branco 0
+#define preto 1
 #define vazio 0
 #define peao 1
 #define torre 2
@@ -12,9 +14,17 @@
 #define rainha 5
 #define rei 6
 
-typedef struct{
+typedef struct casa casa;
+
+typedef struct jogadas_possiveis{
+    casa* ini;
+    casa* fim;
+    struct jogadas_possiveis* prox;
+} lista;
+
+typedef struct casa{
     int peca;
-    bool ameacado;
+    int cor;
 } casa;
 
 typedef struct{
@@ -55,6 +65,36 @@ void jogo_faz_jogada(jogo** j){
     acha_ameacas(c[0]-48, c[1]-97, j, tipo);
 }*/
 
+lista* lista_cria(){
+    lista* l = malloc(sizeof(lista));
+    l->ini = NULL;
+    l->fim = NULL;
+    l->prox = NULL;
+    return l;
+}
+
+void lista_insere(lista* l, casa* ini, casa* fim){
+    lista* novo = lista_cria();
+    novo->ini = ini;
+    novo->fim = fim;
+    lista* aux = l;
+    while(aux->prox != NULL){
+        aux = aux->prox;
+    }
+    aux->prox = novo;
+}
+
+void lista_libera(lista* l){
+    lista* aux = l;
+    while(aux != NULL){
+        lista* aux2 = aux;
+        aux = aux->prox;
+        free(aux2);
+    }
+    free(l);
+}
+
+
 // todas as peças estão colocadas nas posições iniciais corretas
 void tabuleiro_ini(jogo* j){
     int n = 2;
@@ -76,29 +116,38 @@ void tabuleiro_ini(jogo* j){
             j->tabuleiro[k][i].peca = 0;
         }
     }
-    /*for(int i = 0; i < tam_tab; i++){
+}
+
+void imprime_tabuleiro(jogo* j){
+    for(int i = 0; i < tam_tab; i++){
         for(int k = 0; k < tam_tab; k++){
             printf("%d| ", j->tabuleiro[i][k].peca);
         }
         printf("\n");
-    }*/
+    }
 }
 
-void imprime_tabuleiro(jogo* j){
+void libera_tabuleiro(jogo* j){
+    for(int i = 0; i < tam_tab; i++){
+        free(j->tabuleiro[i]);
+    }
+    free(j->tabuleiro);
+    free(j);
+}
 
+void jogo_inicializa(jogo* j){
+    j->tabuleiro = (casa**)malloc(sizeof(casa*)*tam_tab);
+    for(int i = 0; i < tam_tab; i++){
+        j->tabuleiro[i] = (casa*) malloc(sizeof(casa)*tam_tab);
+    }
+    j->fim = 0;
+    j->vez = branco;
 }
 
 int main(){
     jogo* t = malloc(sizeof(jogo));
-    t->tabuleiro = (casa**)malloc(sizeof(casa*)*tam_tab);
-    for(int i = 0; i < tam_tab; i++){
-        t->tabuleiro[i] = (casa*) malloc(sizeof(casa)*tam_tab);
-    }
+    jogo_inicializa(t);
     tabuleiro_ini(t);
-    for (int i = 0; i < tam_tab; i++) {
-        free(t->tabuleiro[i]);
-    }
-    free(t->tabuleiro);
-
-    //free(t);
+    imprime_tabuleiro(t);
+    libera_tabuleiro(t);
 }
